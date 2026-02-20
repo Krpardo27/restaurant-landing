d7bf7d5c587667ebdfd8d851574ddc56862c2729
Krpardo27 <kevin.pardov26@gmail.com>
2026-02-19 22:05:30 -0300

    Se agrega lazy a GallerySlider

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,20 +1,25 @@
-import React from 'react'
-import HeroSlider from '../sections/HeroSlider'
-import Pizzas from '../sections/Pizzas'
-import GallerySlider from '../sections/GallerySlider'
-import About from '../sections/About'
-import Menu from '../sections/Menu'
+import { lazy, Suspense } from "react";
+
+import HeroSlider from "../sections/HeroSlider";
+import Pizzas from "../sections/Pizzas";
+import Menu from "../sections/Menu";
+import About from "../sections/About";
+
+const GallerySlider = lazy(() => import("../sections/GallerySlider"));
 
 const Home = () => {
   return (
     <>
       <HeroSlider />
       <Pizzas />
-      <Menu /> 
+      <Menu />
       <About />
-      <GallerySlider />
-    </>
-  )
-}
 
-export default Home
+      <Suspense fallback={null}>
+        <GallerySlider />
+      </Suspense>
+    </>
+  );
+};
+
+export default Home;
